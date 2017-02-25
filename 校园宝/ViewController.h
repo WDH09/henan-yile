1e998a93cbf6652f3d7d1b7e3bdf99df12983632
wuxian <wuxianlong@wuxiandeMacBook-Air.local>
2017-02-25 22:47:36 +0800

    Initial Commit

---
+++ 校园宝/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  校园宝
+//
+//  Created by wuxian on 2017/2/25.
+//  Copyright © 2017年 HenanYile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 校园宝/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  校园宝
+//
+//  Created by wuxian on 2017/2/25.
+//  Copyright © 2017年 HenanYile. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
